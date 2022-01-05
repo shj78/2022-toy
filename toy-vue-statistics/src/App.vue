3d762fa599b06f10fe7606c0868a62c472d26985
DESKTOP-9C6H7I0\hjsim <hjsim@ecstel.co.kr>
2022-01-05 11:47:44 +0900

    removing cache modify

---
+++ toy-vue-statistics/src/App.vue
@@ -0,0 +1,28 @@
+<!--최상위 컴포넌트--> 
+<template>
+  <div id="app">
+    <title-bar></title-bar>
+    <report-bar></report-bar>
+  </div>
+</template>
+
+<script>
+import TitleBar from './components/TitleBar.vue'
+import ReportBar from './components/ReportBar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    TitleBar, 
+    ReportBar,
+  }
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
